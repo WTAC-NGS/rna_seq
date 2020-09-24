5bd7ff0c92ed42788965ed754f25f61bdfe2b15a
Jacqui Keane <drjkeane@gmail.com>
2020-09-24 13:48:04 +0100

    Update mapping script for fastq.gz files

--- data/map_SBP_samples.sh
+++ data/map_SBP_samples.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for r1 in SBP*_1.fastq
+for r1 in SBP*_1.fastq.gz
 do
   echo $r1
-  sample=${r1/_1.fastq/}
+  sample=${r1/_1.fastq.gz/}
   echo "Processing sample: "$sample
-  hisat2 --max-intronlen 10000 -x PccAS_v3_hisat2.idx -1 $sample'_1.fastq' -2 $sample'_2.fastq' \
+  hisat2 --max-intronlen 10000 -x PccAS_v3_hisat2.idx -1 $sample'_1.fastq.gz' -2 $sample'_2.fastq.gz' \
   | samtools view -b - \
   | samtools sort -o $sample"_sorted.bam" - \
   && samtools index $sample"_sorted.bam" 
