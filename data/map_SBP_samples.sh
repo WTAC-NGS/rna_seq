3537ce22b6758a19f2ab2ccf26aa2b3113a61e4e
Jacqui Keane <drjkeane@gmail.com>
2020-11-30 13:39:17 +0000

    Updated mapping script with correct location of fastq files

--- data/map_SBP_samples.sh
+++ data/map_SBP_samples.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-for r1 in SBP*_1.fastq.gz
+for r1 in data/SBP*_1.fastq.gz
 do
   echo $r1
-  sample=${r1/_1.fastq.gz/}
+  sample=$(basename $r1)
+  sample=${sample/_1.fastq.gz/}
   echo "Processing sample: "$sample
-  hisat2 --max-intronlen 10000 -x PccAS_v3_hisat2.idx -1 $sample'_1.fastq.gz' -2 $sample'_2.fastq.gz' \
+  hisat2 --max-intronlen 10000 -x data/PccAS_v3_hisat2.idx -1 "data/${sample}_1.fastq.gz" -2 "data/${sample}_2.fastq.gz" \
   | samtools view -b - \
-  | samtools sort -o $sample"_sorted.bam" - \
-  && samtools index $sample"_sorted.bam" 
+  | samtools sort -o "data/${sample}_sorted.bam" - \
+  && samtools index "data/${sample}_sorted.bam" 
 done
